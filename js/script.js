b076a24a1d818a419efc3a9a926a4f556ba495f0
GabrielDeFaria <gabrieldelfaria@hotmail.com>
2023-03-23 22:17:54 -0300

    limpeza visual

--- js/script.js
+++ js/script.js
@@ -15,18 +15,3 @@ window.onscroll = () => {
 	menu.classList.remove('bx-x');
 	navlist.classList.remove('open');
 };
-setInterval(function(){
-    
-    let novaHora = new Date();
-    let hora = novaHora.getHours();
-    let minuto = novaHora.getMinutes();
-    let segundo = novaHora.getSeconds();
-    minuto = zero(minuto);
-    segundo = zero(segundo);
-    document.getElementById('hora').textContent = hora+':'+minuto+':'+segundo;
-},1000)
-function zero(x) {
-    if (x < 10) {
-        x = '0' + x;
-    } return x;
-}
